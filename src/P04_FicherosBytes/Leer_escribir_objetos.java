b9cb608fd19e7be1997190a0121c89c298556b2f
Alex <alexnovallas10@gmail.com>
2018-10-10 19:09:05 +0200

    Introducir personas y edades

--- src/P04_FicherosBytes/Leer_escribir_objetos.java
+++ src/P04_FicherosBytes/Leer_escribir_objetos.java
@@ -1,6 +1,5 @@
 package P04_FicherosBytes;
 
-import java.io.DataInputStream;
 import java.io.EOFException;
 import java.io.File;
 import java.io.FileInputStream;
@@ -8,11 +7,14 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.Scanner;
 
 public class Leer_escribir_objetos {
 
 	public static void main(String[] args) throws IOException, ClassNotFoundException {
 		// TODO Auto-generated method stub
+		
+		Scanner teclado = new Scanner(System.in);
 
 		File f = new File("D:\\alex\\Acceso_a_datos\\AD_01_Ficheros\\persona.dat");
 		f.createNewFile();
@@ -21,34 +23,26 @@ public class Leer_escribir_objetos {
 		ObjectOutputStream obout = new ObjectOutputStream(fileout);
 		
 		Persona p1;
-		
-		String[] nombres = new String[10];
-		nombres[0]="Pepe";
-		nombres[1]="Juan";
-		nombres[2]="Jorge";
-		nombres[3]="Luis";
-		nombres[4]="Fran";
-		nombres[5]="Pedro";
-		nombres[6]="Javier";
-		nombres[7]="Ana";
-		nombres[8]="Lucia";
-		nombres[9]="Bea";
-
-		int[] edades = new int[10];
-		edades[0]=19;
-		edades[1]=18;
-		edades[2]=17;
-		edades[3]=21;
-		edades[4]=22;
-		edades[5]=25;
-		edades[6]=26;
-		edades[7]=15;
-		edades[8]=19;
-		edades[9]=17;
-		
 		int i;
 		
-		for(i=0;i<10;i++) {
+		String[] nombres = new String[10];
+		for(i=0;i<5;i++) {
+			System.out.println("Introduce el nombre de la persona: ");	
+			nombres[i]=teclado.nextLine();
+			
+		}
+		
+		
+		int[] edades = new int[10];
+		for(i=0;i<5;i++) {
+			System.out.println("Introduce la edad de la persona: ");	
+			edades[i]=teclado.nextInt();
+		}
+		
+		
+	
+		
+		for(i=0;i<5;i++) {
 			p1 = new Persona(edades[i], nombres[i]);
 			obout.writeObject(p1);
 		
