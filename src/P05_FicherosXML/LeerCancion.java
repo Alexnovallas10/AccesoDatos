cf5787229a331defd9f830db6723e7ade7d17419
Alex <alexnovallas10@gmail.com>
2018-11-05 17:32:53 +0100

    Leer los datos, sin acabar

---
+++ src/P05_FicherosXML/LeerCancion.java
@@ -0,0 +1,36 @@
+package P05_FicherosXML;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import com.thoughtworks.xstream.XStream;
+
+public class LeerCancion {
+
+	public static void main(String[] args) throws IOException, ClassNotFoundException {   	
+		
+	    XStream xstream = new XStream();
+		
+	    xstream.alias("ListaDeCanciones", listaCancion.class);		
+	    //xstream.alias("DatosPersona", cancion.class);	
+	    //xstream.addImplicitCollection(listaCancion.class, "lista");
+		
+	    listaCancion listadoTodas = (listaCancion) 
+	         xstream.fromXML(new FileInputStream("D:\\alex\\Acceso_a_datos\\AD_01_Ficheros\\ListaCanciones.xml"));			
+	    System.out.println("Numero de Canciones: " + 
+	         listadoTodas.getListaCanciones().size());
+				       
+	    List<cancion> ListaCancion = new ArrayList<cancion>();
+		ListaCancion = listadoTodas.getListaCanciones();
+				        
+	    Iterator <cancion> iterador = ListaCancion.listIterator(); 
+	    while( iterador.hasNext() ) {
+		    cancion c = (cancion) iterador.next(); 
+		    System.out.printf("ID: "+c.getId()+"Año: "+c.getAnio()+"Titulo: "+c.getTitulo()+" Artista: "+ c.getArtista()+"Duracion: "+c.getDuracion()+c.getD());
+	                                 
+	    }    
+	    System.out.println("Fin de listado .....");
+	 } //fin main
+	
+}
